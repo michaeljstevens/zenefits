fc07f084d38d77aeef76eccc890f632856b138a1
michaeljstevens <michaeljstevens@zoho.com>
2016-12-12 02:25:03 -0800

    places search box works

--- js/bundle.js
+++ js/bundle.js
@@ -21583,6 +21583,45 @@
 	
 	      var markers = [];
 	
+	      searchBox.addListener('places_changed', function () {
+	        var places = searchBox.getPlaces();
+	
+	        if (places.length === 0) return;
+	
+	        markers.forEach(function (marker) {
+	          marker.setMap(null);
+	        });
+	        markers = [];
+	
+	        var bounds = new google.maps.LatLngBounds();
+	        places.forEach(function (place) {
+	          if (!place.geometry) return;
+	
+	          var icon = {
+	            url: place.icon,
+	            size: new google.maps.Size(71, 71),
+	            origin: new google.maps.Point(0, 0),
+	            anchor: new google.maps.Point(17, 34),
+	            scaledSize: new google.maps.Size(25, 25)
+	          };
+	
+	          markers.push(new google.maps.Marker({
+	            map: map,
+	            icon: icon,
+	            title: place.name,
+	            position: place.geometry.location
+	          }));
+	
+	          if (place.geometry.viewport) {
+	            // Only geocodes have viewport.
+	            bounds.union(place.geometry.viewport);
+	          } else {
+	            bounds.extend(place.geometry.location);
+	          }
+	        });
+	        map.fitBounds(bounds);
+	      });
+	
 	      if (navigator.geolocation) {
 	        navigator.geolocation.getCurrentPosition(function (position) {
 	          var pos = {
--- js/map.jsx
+++ js/map.jsx
@@ -26,6 +26,46 @@ class Map extends Component {
 
     let markers = [];
 
+    searchBox.addListener('places_changed', () => {
+      const places = searchBox.getPlaces();
+
+      if(places.length === 0) return;
+
+      markers.forEach(marker => {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach(place => {
+        if(!place.geometry) return;
+
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+
+
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         const pos = {
